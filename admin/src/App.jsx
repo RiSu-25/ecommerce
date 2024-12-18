11acb99e0f5b82a435bbb90f81179d81a2a99537
Rishita Sarkar <eagerrishita@gmail.com>
2024-12-18 13:15:57 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
         <hr />
         <div className='flex w-full'>
           <Sidebar />
-          <div className='w-[70%] ml-[max(95w,25px)] my-8 text-gray-600 text-base'>
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
             <Routes>
               <Route path='/add' element={<Add />} />
               <Route path='/list' element={<List />} />
